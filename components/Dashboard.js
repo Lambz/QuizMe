1119b65aff161ac07a88bc6f87d9b08e93f8715e
chetu97 <chaitanya.sanoriya@gmail.com>
2021-04-14 18:27:18 -0400

    added structure to the app

---
+++ components/Dashboard.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./Home";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import UserTab from "./UserTab";
+import Browse from "./Browse";
+import Create from "./Create";
+
+const Tab = createBottomTabNavigator();
+export default function Dashboard() {
+    return (
+        <NavigationContainer independent={true}>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        tabBarLabel: "Home",
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="home"
+                                color={color}
+                                size={size}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Browse"
+                    component={Browse}
+                    // initialParams={{
+                    //     stackMoveCallback: stackMoveCallback,
+                    //     setFocusFunction: setFocusFunction,
+                    //     deRegisterFocus: deRegisterFocus,
+                    // }}
+                    options={{
+                        tabBarLabel: "Browse",
+                        tabBarIcon: ({ color, size }) => (
+                            <Feather name="box" size={size} color={color} />
+                        ),
+                    }}
+                />
+
+                <Tab.Screen
+                    name="Create"
+                    component={Create}
+                    // initialParams={{
+                    //     stackMoveCallback: stackMoveCallback,
+                    //     setFocusFunction: setFocusFunction,
+                    //     deRegisterFocus: deRegisterFocus,
+                    // }}
+                    options={{
+                        tabBarLabel: "Create",
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons
+                                name="create-outline"
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="UserTab"
+                    component={UserTab}
+                    // initialParams={{ stackMoveCallback: stackMoveCallback }}
+                    options={{
+                        tabBarLabel: "User",
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons
+                                name="account-circle-outline"
+                                color={color}
+                                size={size}
+                            />
+                        ),
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
