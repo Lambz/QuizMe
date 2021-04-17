628cf117346e8daeb7d302412428a18b3f9a59fa
chetu97 <chaitanya.sanoriya@gmail.com>
2021-04-17 00:30:05 -0400

    update on app

--- App.js
+++ App.js
@@ -3,17 +3,11 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Dashboard from "./components/Dashboard";
 import Quiz from "./components/Quiz";
-import AuthScreen from "./containers/AuthScreen";
 const Stack = createStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen
-                    name="Auth"
-                    component={AuthScreen}
-                    options={{ headerShown: false }}
-                />
                 <Stack.Screen
                     name="dashboard"
                     component={Dashboard}
--- components/Home.js
+++ components/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { useFocusEffect } from "@react-navigation/core";
 import { FlatList } from "react-native-gesture-handler";
+import { AuthSession, WebBrowser, Linking } from "expo";
 import QuizItem from "./QuizItem";
 export default function Home({ route }) {
     const [quizes, setQuizes] = useState([
@@ -33,8 +34,49 @@ export default function Home({ route }) {
         console.log(quiz);
         route.params.moveTo("Quiz", { quiz: quiz });
     };
+
+    const handleRedirect = async (event) => {
+        WebBrowser.dismissBrowser();
+    };
+
+    const handleOAuthLogin = async () => {
+        // gets the app's deep link
+        let redirectUrl = await Linking.getInitialURL();
+        console.log(redirectUrl);
+        // this should change depending on where the server is running
+        let authUrl = `http://localhost:3000/auth/google`;
+        addLinkingListener();
+        try {
+            let authResult = await WebBrowser.openAuthSessionAsync(
+                `http://localhost:3000/auth/google`,
+                redirectUrl
+            );
+            await this.setState({ authResult: authResult });
+        } catch (err) {
+            console.log("ERROR:", err);
+        }
+        removeLinkingListener();
+    };
+    const addLinkingListener = () => {
+        Linking.addEventListener("url", handleRedirect);
+    };
+    const removeLinkingListener = () => {
+        Linking.removeEventListener("url", handleRedirect);
+    };
+
+    const sendRequest = async () => {
+        try {
+            let response = await fetch("http://localhost:3000/auth/google");
+            console.log(response);
+            let json = await response.json();
+            console.log("JSON: ", json);
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    };
     return (
         <View style={styles.container}>
+            <Button title="Login" onPress={handleOAuthLogin} />
             <FlatList
                 data={quizes}
                 renderItem={({ item }) => (
