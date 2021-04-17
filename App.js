8d3887d6edb8f13d6d7f2d6d33e3817f29b41817
chetu97 <chaitanya.sanoriya@gmail.com>
2021-04-16 22:14:44 -0400

    process of auth screen

--- App.js
+++ App.js
@@ -3,11 +3,17 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Dashboard from "./components/Dashboard";
 import Quiz from "./components/Quiz";
+import AuthScreen from "./containers/AuthScreen";
 const Stack = createStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen
+                    name="Auth"
+                    component={AuthScreen}
+                    options={{ headerShown: false }}
+                />
                 <Stack.Screen
                     name="dashboard"
                     component={Dashboard}
